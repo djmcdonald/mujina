fb814dbda09b8506fddfe329fd6686f1aa43877b
Duncan McDonald <duncan.mcdonald@bbc.co.uk>
2014-02-05 19:24:35 +0000

    Use model to generate time ranges.

--- mujina/public/javascript/app.js
+++ mujina/public/javascript/app.js
@@ -3,37 +3,50 @@
     _.templateSettings.variable = "rc";
 
 
-    Test = Backbone.Model.extend({
-        urlRoot: "/api/tv/guide/channels",
-        defaults : {
-            id: null
+    var Guide = Backbone.Model.extend({
+        defaults: {
+            duration: 1,
+            end_time: null,
+            start_time: null
+        },
+        time_ranges: function(){
+            var start = moment(this.get('start_time'), 'YYYY-MM-DD HH:mm:ss');
+            var interval = this.get('duration')
+            // Fixed at four intervals for now.
+            return [
+                start.format('HH:mm'),
+                start.add('seconds', interval).format('HH:mm'),
+                start.add('seconds', interval * 2).format('HH:mm'),
+                start.add('seconds', interval * 3).format('HH:mm')
+            ];
         }
-    })
+    });
 
-    var Guide = {};
+    var GuideCollection = {};
 
-    Guide.Collection = Backbone.Collection.extend({
+    GuideCollection.Collection = Backbone.Collection.extend({
+        model:Guide,
         url: '/api/tv/guide/channels'
     });
 
-    Guide.Views = {};
+    GuideCollection.Views = {};
 
-    Guide.Views.List = Backbone.View.extend({
+    GuideCollection.Views.List = Backbone.View.extend({
         initialize: function () {
             this.collection.bind('add', this.render, this);
         },
         render: function () {
             this.collection.each(function (model) {
                 _.extend(model, viewHelpers);
-                var template = _.template( $("#channels-template").html(), model.toJSON());
+                var template = _.template( $("#channels-template").html(), model);
                 $('#tv-guide').html( template );
             }, this);
         }
     });
 
     $(function () {
-        var collection = new Guide.Collection(),
-            view = new Guide.Views.List({
+        var collection = new GuideCollection.Collection(),
+            view = new GuideCollection.Views.List({
                 collection: collection
             });
 
