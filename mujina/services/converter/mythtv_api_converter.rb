e33d1f95efd7a54ed51b9b4b66ce9705650ef845
Duncan McDonald <duncan.mcdonald@bbc.co.uk>
2014-02-05 20:17:57 +0000

    Parse shows returned.

--- mujina/services/converter/mythtv_api_converter.rb
+++ mujina/services/converter/mythtv_api_converter.rb
@@ -1,20 +1,35 @@
 require 'json'
 require 'model/mujina/channel'
 require 'model/mujina/channels'
+require 'model/mujina/show'
 
 class MythTVAPIConverter
   def tv_guide(start_date = nil)
     guide = Channels.new start_date
 
     response_body = File.read("fixtures/tv_guide.json")
-    mythtv_response = JSON.parse(response_body)
+    mythtv_response = JSON[response_body]
 
     sorted_channel_list = mythtv_response['ProgramGuide']['Channels'].sort_by{ |channel| channel['ChanNum'].to_i }
-    sorted_channel_list.each do |channel|
-      channel = Channel.new(channel['ChanId'], channel['ChanNum'], channel['CallSign'])
+    sorted_channel_list.each do |sorted_channel|
+      channel = Channel.new(sorted_channel['ChanId'], sorted_channel['ChanNum'], sorted_channel['CallSign'])
+
+      sorted_channel['Programs'].each do |program|
+        #(id, start_time, end_time, title, description, category, repeat)
+        show = Show.new(
+          1,
+          program['StartTime'],
+          program['EndTime'],
+          program['Title'],
+          program['SubTitle'],
+          program['Category'],
+          program['Repeat'])
+        channel.add_show show
+      end
+
       guide.add_channel channel
     end
 
-    JSON.dump(guide)
+    JSON[guide]
   end
 end
